3625e78c12db60b4cabcb5712c1f5e753109becb
Dalsen Chung <Codedalsen@163.com>
2018-04-15 14:17:06 +0800

    commit files

--- data-map-sys/src/App.vue
+++ data-map-sys/src/App.vue
@@ -12,16 +12,16 @@ export default {
 </script>
 
 <style lang="scss" scoped type="text/css">
-// #app {
-//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-//   margin-top: 60px;
-// }
-// $background:#cccccc;
-body{
-  background-color: red;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
+// $background:#cccccc;
+// body{
+//   background-color: red;
+// }
 </style>
