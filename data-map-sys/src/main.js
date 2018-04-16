8e2b1dae8d7bd04a38afdebaa1d90b59f7db3930
Dalsen <Codedalsen@163.com>
2018-04-16 09:51:00 +0800

    commit files

--- data-map-sys/src/main.js
+++ data-map-sys/src/main.js
@@ -3,8 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
+import ElementUI from 'element-ui'   //按需引入
+import axios from 'axios'
+import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'    //按需引入
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+Vue.prototype.$echarts = echarts
+Vue.use(ElementUI);
+Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
